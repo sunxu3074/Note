4e344daeb3d3484ef154c69954e857dde218d88b
sunxu3074 <393353762@qq.com>
2015-09-22 11:33:19 +0800

    Update

---
+++ 读书笔记/developer_android_com_training/Create_a_Fragment.java
@@ -0,0 +1,8 @@
+
+
+
+		Creating a Fragment
+
+		//如果你的应用只需要11以上 不需要导入v4包 直接导入android.app.Fragment即可(为了兼容性能可能会差一点点?)
+
+		
--- 读书笔记/developer_android_com_training/Managing_the_Activity_LifeCycle.java
+++ 读书笔记/developer_android_com_training/Managing_the_Activity_LifeCycle.java
@@ -74,3 +74,70 @@
 				        initializeCamera(); // Local method to handle camera init
 				    }
 				}
+
+	// 如果主动调用finish()方法 会直接回调onDestroy(); 不执行之间的一些方法
+
+
+
+    Stopping and Restarting an Activity
+
+    	Stop Your Activity
+
+    	//在极端情况下,系统不会回调onDestroy()方法而直接杀死应用进程,在onStop()方法中释放会导致内存泄露的资源是非常重要的。
+
+    	should use onStop() to perform larger, more CPU intensive shut-down operations, such as writing information to a database.
+
+    	Start/Restart Your Activity
+
+    	// 在onStart()方法中做一些回复工作.(即使avtivity的实例被销毁了,onRestart()做不到)  
+    	// 跟onStop()对应
+    	//一般来说onStop()就释放了大部分的资源,OnDestroy()是清除可能会导致内存泄露的最后一次机会.( be sure that additional threads are destroyed and other long-running actions like method tracing are also stopped.)
+
+
+    Recreating an Activity
+
+    	// 每个view最好加上id 尤其是能编辑的内容 便于毁容 给用户更好的体验
+    	In order for the Android system to restore the state of the views in your activity, each view must have a unique ID, supplied by the android:id attribute.
+
+
+
+    	static final String STATE_SCORE = "playerScore";
+		static final String STATE_LEVEL = "playerLevel";
+		... 
+		 
+		@Override 
+		public void onSaveInstanceState(Bundle savedInstanceState) {
+		    // Save the user's current game state 
+		    savedInstanceState.putInt(STATE_SCORE, mCurrentScore);
+		    savedInstanceState.putInt(STATE_LEVEL, mCurrentLevel);
+		     
+		    // Always call the superclass so it can save the view hierarchy state 
+		    super.onSaveInstanceState(savedInstanceState);
+		} 
+
+
+		@Override 
+		protected void onCreate(Bundle savedInstanceState) {
+		    super.onCreate(savedInstanceState); // Always call the superclass first
+		    
+		    // Check whether we're recreating a previously destroyed instance 
+		    if (savedInstanceState != null) {
+		        // Restore value of members from saved state 
+		        mCurrentScore = savedInstanceState.getInt(STATE_SCORE);
+		        mCurrentLevel = savedInstanceState.getInt(STATE_LEVEL);
+		    } else { 
+		        // Probably initialize members with default values for a new instance 
+		    } 
+		    ... 
+		} 
+
+
+		// 在onStart()方法之后执行,且不用验证Bundle是否为空,只有Bundle不为空时才会调用该方法
+		public void onRestoreInstanceState(Bundle savedInstanceState) {
+		    // Always call the superclass so it can restore the view hierarchy 
+		    super.onRestoreInstanceState(savedInstanceState);
+		    
+		    // Restore state members from saved instance 
+		    mCurrentScore = savedInstanceState.getInt(STATE_SCORE);
+		    mCurrentLevel = savedInstanceState.getInt(STATE_LEVEL);
+		} 
