e4e584aa16f1f76178c6401935639bdfd651823d
sunxu3074 <393353762@qq.com>
2015-09-28 11:12:58 +0800

    Update

--- 读书笔记/developer_android_com_training/Building Apps With Content Sharing/Sharing_Simple_Data.java
+++ 读书笔记/developer_android_com_training/Building Apps With Content Sharing/Sharing_Simple_Data.java
@@ -42,3 +42,137 @@
 			shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris); 
 			shareIntent.setType("image/*"); 
 			startActivity(Intent.createChooser(shareIntent, "Share images to.."));
+
+
+
+	Receiving Simple Data from Other Apps
+
+		Update Your Manifest
+
+		<activity android:name=".ui.MyActivity" >
+		    <intent-filter>
+		        <action android:name="android.intent.action.SEND" />
+		        <category android:name="android.intent.category.DEFAULT" />
+		        <data android:mimeType="image/*" />
+		    </intent-filter>
+		    <intent-filter>
+		        <action android:name="android.intent.action.SEND" />
+		        <category android:name="android.intent.category.DEFAULT" />
+		        <data android:mimeType="text/plain" />
+		    </intent-filter>
+		    <intent-filter>
+		        <action android:name="android.intent.action.SEND_MULTIPLE" />
+		        <category android:name="android.intent.category.DEFAULT" />
+		        <data android:mimeType="image/*" />
+		    </intent-filter>
+		</activity>
+
+
+		Handle the Incoming Content
+
+
+		void onCreate (Bundle savedInstanceState) {
+		    ... 
+		    // Get intent, action and MIME type 
+		    Intent intent = getIntent();
+		    String action = intent.getAction();
+		    String type = intent.getType();
+		 
+		    if (Intent.ACTION_SEND.equals(action) && type != null) {
+		        if ("text/plain".equals(type)) {
+		            handleSendText(intent); // Handle text being sent
+		        } else if (type.startsWith("image/")) {
+		            handleSendImage(intent); // Handle single image being sent
+		        } 
+		    } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+		        if (type.startsWith("image/")) {
+		            handleSendMultipleImages(intent); // Handle multiple images being sent
+		        } 
+		    } else { 
+		        // Handle other intents, such as being started from the home screen 
+		    } 
+		    ... 
+		} 
+		 
+		void handleSendText(Intent intent) {
+		    String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		    if (sharedText != null) {
+		        // Update UI to reflect text being shared 
+		    } 
+		} 
+		 
+		void handleSendImage(Intent intent) {
+		    Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+		    if (imageUri != null) {
+		        // Update UI to reflect image being shared 
+		    } 
+		} 
+		 
+		void handleSendMultipleImages(Intent intent) {
+		    ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+		    if (imageUris != null) {
+		        // Update UI to reflect multiple images being shared 
+		    } 
+		} 
+		//remember to process binary data in a separate thread rather than the main ("UI") thread.
+
+
+
+
+	Adding an Easy Share Action
+
+		 ShareActionProvider is available starting with API Level 14 and higher.
+
+		 Update Menu Declarations
+
+		 <menu xmlns:android="http://schemas.android.com/apk/res/android">
+		    <item
+		            android:id="@+id/menu_item_share"
+		            android:showAsAction="ifRoom"
+		            android:title="Share"
+		            android:actionProviderClass=
+		                "android.widget.ShareActionProvider" />
+		    ...
+		</menu>
+
+		Set the Share Intent
+
+		private ShareActionProvider mShareActionProvider;
+		... 
+		 
+		@Override 
+		public boolean onCreateOptionsMenu(Menu menu) {
+		    // Inflate menu resource file. 
+		    getMenuInflater().inflate(R.menu.share_menu, menu);
+		 
+		    // Locate MenuItem with ShareActionProvider 
+		    MenuItem item = menu.findItem(R.id.menu_item_share);
+		 
+		    // Fetch and store ShareActionProvider 
+		    mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+		 
+		    // Return true to display menu 
+		    return true; 
+		} 
+		 
+		// Call to update the share intent 
+		private void setShareIntent(Intent shareIntent) {
+		    if (mShareActionProvider != null) {
+		        mShareActionProvider.setShareIntent(shareIntent);
+		    } 
+		} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
