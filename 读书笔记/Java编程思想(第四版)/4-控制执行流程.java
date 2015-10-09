23dbc0f3e4afda1b8038a9f45c1214dcca8a37b0
sunxu3074 <393353762@qq.com>
2015-10-09 11:05:27 +0800

    Update

---
+++ 读书笔记/Java编程思想(第四版)/4-控制执行流程.java
@@ -0,0 +1,29 @@
+
+
+		1.
+		char c = 'a';
+		Character.isLowerCase(c)方法检查c是不是小写字母.
+
+		2.逗号操作符
+
+		3.String类中有一个方法toCharArray()返回一个char数组.
+
+		4.net.mindview.util.Range包中创建了一个名为range()的方法.
+		  for(int i ,range(10)) // 0...9
+		  for(int i ,range(5,10)) // 5,6,7,8,9
+		  for(int i,range(5,20,3)) //5--20 step 3
+		  // foreach + range 效率也许会降低
+		  // foreach语法不仅在录入代码时可以节省时间,更重要的是,它阅读起来也要容易得多,它说明你在努力做什么(例如获取数组中的每一个元素),而不是给出你正在如何做的细节.
+
+		5.break和continue
+		  break用于强行退出循环,不执行循环中剩余的语句.
+		  continue则停止执行当前的迭代,然后退回循环起始处,继续下一次迭代.
+
+		6.while(true) 和 for(;;) 这两种无穷循环的格式,更喜欢后一种// :)
+
+		7.switch 
+		  // int char enum String(java 7)
+		  // 如果考虑到代码风格方面的原因,完全可以再default语句的末尾放置一个break,尽管它没有任何实际的用处.
+				  	
+
+		  
