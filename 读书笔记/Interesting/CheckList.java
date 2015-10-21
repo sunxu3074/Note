0aeb60b5331be69dcd49bf58f9506f98493d05e4
sunxu3074 <393353762@qq.com>
2015-10-22 01:15:55 +0800

    Update

--- 读书笔记/Interesting/CheckList.java
+++ 读书笔记/Interesting/CheckList.java
@@ -2,3 +2,19 @@
 
 	
 		1.Adapter 中为什么不建议加监听器, 那怎么做更好?
+
+
+		2.layout_weight及常见属性解析
+
+		  1>TextView 在LinearLayout中是第一行字是基于LinearLayout的基线来对齐的.如果不想让其对齐,可以在LinrearLayout中设置android:baselineAligned="false"；
+		  2>LinearLayout中三个TextView,第一个TextView的width设置为wrap_content,另外两个设置成0dp,weight分别是1,2,2.
+		  	// 宽分别是wrap_content+剩下的1/5,剩下的2/5,剩下的2/5
+		  3>LinearLayout中三个TextView,三个TextView的width设置为match_parent,weight分别是1,2,2.
+		  	// 宽分别是198 96 96(假设LinearLayout的宽是480dp)
+          weight:首先按照控件声明的尺寸进行分配,然后剩下的尺寸按照weight分配.(控件宽度+父控件剩余宽度*比例)
+          4>LinearLayout中中只有一个TextView,需要设置为父控件的1/2,可以在LinrearLayout中设置android:weightSum="2";TextView的weight设为1即可.
+          5>layout_ 这种属性是相对于父控件的.例如layout_gravity是在父控件的布局,而gravity是控件本身内部的布局.
+
+		3.在开发过程中是否自定义过控件,如何自定义控件
+
+		4.ListView的性能优化
