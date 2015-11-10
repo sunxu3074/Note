b66d62bad443eac512fefeaf5ccb5ea0da683274
sunxu3074 <393353762@qq.com>
2015-11-10 10:34:08 +0800

    Create Supporting Different Devices

---
+++ 读书笔记/developer_android_com_training/Getting Started/Supporting Different Devices.java
@@ -0,0 +1,20 @@
+
+
+		#Supporting Different Languages
+
+		MyProject/
+		    res/
+		       - values/
+		           strings.xml
+		       - values-es/
+		           strings.xml
+		       - values-fr/
+		           strings.xml
+		       - values-zh-rCN
+		           strings.xml
+
+		- - 
+
+
+
+		
