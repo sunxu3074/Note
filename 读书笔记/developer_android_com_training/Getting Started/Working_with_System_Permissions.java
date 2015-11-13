44cf35edf9f1af196182c18a8e8a17748b97c3e8
sunxu3074 <393353762@qq.com>
2015-11-13 18:32:47 +0800

    Update

--- 读书笔记/developer_android_com_training/Getting Started/Working_with_System_Permissions.java
+++ 读书笔记/developer_android_com_training/Getting Started/Working_with_System_Permissions.java
@@ -14,7 +14,7 @@
 		// 你的应用程序只需要直接执行的权限(如果在app中读取联系人则需要读取联系人的权限,如果是启动别的app,如系统自带的联系人app从而获得联系人信息,则不需要任何权限,但是系统自带的app需要权限)
 		For more information, see Consider Using an Intent
 		http://developer.android.com/training/permissions/best-practices.html#perms-vs-intents
-    ------------------------------------------------------------------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------------------------------------------------------------
 
     	Requesting Permissions at Run Time
 
@@ -46,9 +46,67 @@
 		//弹出一个dialog解释为什么需要这个权限(简短有力的解释)
 
 		shouldShowRequestPermissionRationale()
-		return true; --> 
+		return true; --> 之前请求过但是被拒绝请求了
+		return false; --> 之前被拒绝还被点击了 Don't' ask again   或者  设备不支持
 
 
+		Request the permissions you need
 
-		
+		// Here, thisActivity is the current activity 
+		if (ContextCompat.checkSelfPermission(thisActivity,
+		                Manifest.permission.READ_CONTACTS) 
+		        != PackageManager.PERMISSION_GRANTED) {
+		 
+		    // Should we show an explanation? 
+		    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+		            Manifest.permission.READ_CONTACTS)) { 
+		 
+		        // Show an expanation to the user *asynchronously* -- don't block 
+		        // this thread waiting for the user's response! After the user 
+		        // sees the explanation, try again to request the permission. 
+		 
+		    } else { 
+		 
+		        // No explanation needed, we can request the permission. 
+		 
+		        ActivityCompat.requestPermissions(thisActivity,
+		                new String[]{Manifest.permission.READ_CONTACTS},
+		                MY_PERMISSIONS_REQUEST_READ_CONTACTS); 
+		 
+		        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an 
+		        // app-defined int constant. The callback method gets the 
+		        // result of the request. 
+		    } 
+		} 
+
+
+		//The callback is passed the same request code you passed to requestPermissions()
+		@Override 
+		public void onRequestPermissionsResult(int requestCode,
+		        String permissions[], int[] grantResults) {
+		    switch (requestCode) {
+		        case MY_PERMISSIONS_REQUEST_READ_CONTACTS: { 
+		            // If request is cancelled, the result arrays are empty. 
+		            if (grantResults.length > 0
+		                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+		 
+		                // permission was granted, yay! Do the 
+		                // contacts-related task you need to do. 
+		 
+		            } else { 
+		 
+		                // permission denied, boo! Disable the 
+		                // functionality that depends on this permission. 
+		            } 
+		            return; 
+		        } 
+		 
+		        // other 'case' lines to check for other 
+		        // permissions this app might request 
+		    } 
+		} 
+
+	-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+		Permissions Best Practices
 				
